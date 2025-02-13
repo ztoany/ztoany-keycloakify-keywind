20629412d77ad0d7809f55b8b8441787abac25a6
ztoany <marvin.m.huang@outlook.com>
2025-02-13 11:56:22 +0800

    apply keywind to Terms

--- src/login/pages/Terms.tsx
+++ src/login/pages/Terms.tsx
@@ -1,17 +1,13 @@
-import { getKcClsx } from "keycloakify/login/lib/kcClsx";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
+import ButtonGroup from "../components/ButtonGroup";
+import SubmitButton from "../components/SubmitButton";
 import type { I18n } from "../i18n";
 
 export default function Terms(props: PageProps<Extract<KcContext, { pageId: "terms.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
 
-    const { kcClsx } = getKcClsx({
-        doUseDefaultCss,
-        classes
-    });
-
-    const { msg, msgStr } = i18n;
+    const { msg } = i18n;
 
     const { url } = kcContext;
 
@@ -25,23 +21,24 @@ export default function Terms(props: PageProps<Extract<KcContext, { pageId: "ter
             headerNode={msg("termsTitle")}
         >
             <div id="kc-terms-text">{msg("termsText")}</div>
-            <form className="form-actions" action={url.loginAction} method="POST">
-                <input
-                    className={kcClsx("kcButtonClass", "kcButtonClass", "kcButtonClass", "kcButtonPrimaryClass", "kcButtonLargeClass")}
-                    name="accept"
-                    id="kc-accept"
-                    type="submit"
-                    value={msgStr("doAccept")}
-                />
-                <input
-                    className={kcClsx("kcButtonClass", "kcButtonDefaultClass", "kcButtonLargeClass")}
-                    name="cancel"
-                    id="kc-decline"
-                    type="submit"
-                    value={msgStr("doDecline")}
-                />
+            <form className="form-actions formSpaceClass" action={url.loginAction} method="POST">
+                <ButtonGroup>
+                    <>
+                        <SubmitButton doUseDefaultCss={doUseDefaultCss} classes={classes} id="kc-accept" name="accept">
+                            {msg("doAccept")}
+                        </SubmitButton>
+                        <SubmitButton
+                            doUseDefaultCss={doUseDefaultCss}
+                            classes={classes}
+                            id="kc-decline"
+                            name="cancel"
+                            colorClass="buttonSecondaryClass"
+                        >
+                            {msg("doDecline")}
+                        </SubmitButton>
+                    </>
+                </ButtonGroup>
             </form>
-            <div className="clearfix" />
         </Template>
     );
 }
