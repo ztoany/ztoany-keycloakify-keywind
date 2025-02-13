d9a754ec4b2e8022404ea737376538eaf493060d
ztoany <marvin.m.huang@outlook.com>
2025-02-13 10:25:29 +0800

    apply keywind to LogoutConfirm

--- src/login/pages/LogoutConfirm.tsx
+++ src/login/pages/LogoutConfirm.tsx
@@ -1,48 +1,44 @@
-import { getKcClsx } from "keycloakify/login/lib/kcClsx";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
+import ButtonGroup from "../components/ButtonGroup";
+import Link from "../components/Link";
+import SubmitButton from "../components/SubmitButton";
 import type { I18n } from "../i18n";
 
 export default function LogoutConfirm(props: PageProps<Extract<KcContext, { pageId: "logout-confirm.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
 
-    const { kcClsx } = getKcClsx({
-        doUseDefaultCss,
-        classes
-    });
-
     const { url, client, logoutConfirm } = kcContext;
 
-    const { msg, msgStr } = i18n;
+    const { msg } = i18n;
 
     return (
         <Template kcContext={kcContext} i18n={i18n} doUseDefaultCss={doUseDefaultCss} classes={classes} headerNode={msg("logoutConfirmTitle")}>
             <div id="kc-logout-confirm" className="content-area">
                 <p className="instruction">{msg("logoutConfirmHeader")}</p>
-                <form className="form-actions" action={url.logoutConfirmAction} method="POST">
-                    <input type="hidden" name="session_code" value={logoutConfirm.code} />
-                    <div className={kcClsx("kcFormGroupClass")}>
-                        <div id="kc-form-options">
-                            <div className={kcClsx("kcFormOptionsWrapperClass")}></div>
-                        </div>
-                        <div id="kc-form-buttons" className={kcClsx("kcFormGroupClass")}>
-                            <input
-                                tabIndex={4}
-                                className={kcClsx("kcButtonClass", "kcButtonPrimaryClass", "kcButtonBlockClass", "kcButtonLargeClass")}
-                                name="confirmLogout"
-                                id="kc-logout"
-                                type="submit"
-                                value={msgStr("doLogout")}
-                            />
-                        </div>
+                <div className="m-0 space-y-4">
+                    <form className="form-actions formSpaceClass" action={url.logoutConfirmAction} method="POST">
+                        <input type="hidden" name="session_code" value={logoutConfirm.code} />
+                        <ButtonGroup>
+                            <SubmitButton tabIndex={4} id="kc-logout" name="confirmLogout" doUseDefaultCss={doUseDefaultCss} classes={classes}>
+                                {msg("doLogout")}
+                            </SubmitButton>
+                        </ButtonGroup>
+                    </form>
+                    <div id="kc-info-message">
+                        {!logoutConfirm.skipLink && client.baseUrl && (
+                            <p>
+                                <Link
+                                    id="backToApplication"
+                                    href={client.baseUrl}
+                                    colorClass="linkSecondaryClass"
+                                    fontSizeClass="linkFontSmallSizeClass"
+                                >
+                                    {msg("backToApplication")}
+                                </Link>
+                            </p>
+                        )}
                     </div>
-                </form>
-                <div id="kc-info-message">
-                    {!logoutConfirm.skipLink && client.baseUrl && (
-                        <p>
-                            <a href={client.baseUrl}>{msg("backToApplication")}</a>
-                        </p>
-                    )}
                 </div>
             </div>
         </Template>
