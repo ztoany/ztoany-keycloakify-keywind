0c5cbc9de1595c9aacbd14ecc9ca4570dfea7357
ztoany <marvin.m.huang@outlook.com>
2025-02-13 20:21:52 +0800

    apply keywind to LoginX509Info

--- src/login/pages/LoginX509Info.tsx
+++ src/login/pages/LoginX509Info.tsx
@@ -1,6 +1,9 @@
 import { getKcClsx } from "keycloakify/login/lib/kcClsx";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
+import { clsx } from "keycloakify/tools/clsx";
 import type { KcContext } from "../KcContext";
+import ButtonGroup from "../components/ButtonGroup";
+import SubmitButton from "../components/SubmitButton";
 import type { I18n } from "../i18n";
 
 export default function LoginX509Info(props: PageProps<Extract<KcContext, { pageId: "login-x509-info.ftl" }>, I18n>) {
@@ -17,68 +20,61 @@ export default function LoginX509Info(props: PageProps<Extract<KcContext, { page
 
     return (
         <Template kcContext={kcContext} i18n={i18n} doUseDefaultCss={doUseDefaultCss} classes={classes} headerNode={msg("doLogIn")}>
-            <form id="kc-x509-login-info" className={kcClsx("kcFormClass")} action={url.loginAction} method="post">
-                <div className={kcClsx("kcFormGroupClass")}>
+            <div className={kcClsx("kcFormGroupClass")}>
+                <div className={kcClsx("kcLabelWrapperClass")}>
+                    <label htmlFor="certificate_subjectDN" className={kcClsx("kcLabelClass")}>
+                        {msg("clientCertificate")}
+                    </label>
+                </div>
+                {x509.formData.subjectDN ? (
                     <div className={kcClsx("kcLabelWrapperClass")}>
-                        <label htmlFor="certificate_subjectDN" className={kcClsx("kcLabelClass")}>
-                            {msg("clientCertificate")}
+                        <label id="certificate_subjectDN" className={kcClsx("kcLabelClass")}>
+                            {x509.formData.subjectDN}
                         </label>
                     </div>
-                    {x509.formData.subjectDN ? (
+                ) : (
+                    <div className={kcClsx("kcLabelWrapperClass")}>
+                        <label id="certificate_subjectDN" className={kcClsx("kcLabelClass")}>
+                            {msg("noCertificate")}
+                        </label>
+                    </div>
+                )}
+            </div>
+            <div className={kcClsx("kcFormGroupClass")}>
+                {x509.formData.isUserEnabled && (
+                    <>
                         <div className={kcClsx("kcLabelWrapperClass")}>
-                            <label id="certificate_subjectDN" className={kcClsx("kcLabelClass")}>
-                                {x509.formData.subjectDN}
+                            <label htmlFor="username" className={kcClsx("kcLabelClass")}>
+                                {msg("doX509Login")}
                             </label>
                         </div>
-                    ) : (
                         <div className={kcClsx("kcLabelWrapperClass")}>
-                            <label id="certificate_subjectDN" className={kcClsx("kcLabelClass")}>
-                                {msg("noCertificate")}
+                            <label id="username" className={kcClsx("kcLabelClass")}>
+                                {x509.formData.username}
                             </label>
                         </div>
-                    )}
-                </div>
-                <div className={kcClsx("kcFormGroupClass")}>
-                    {x509.formData.isUserEnabled && (
-                        <>
-                            <div className={kcClsx("kcLabelWrapperClass")}>
-                                <label htmlFor="username" className={kcClsx("kcLabelClass")}>
-                                    {msg("doX509Login")}
-                                </label>
-                            </div>
-                            <div className={kcClsx("kcLabelWrapperClass")}>
-                                <label id="username" className={kcClsx("kcLabelClass")}>
-                                    {x509.formData.username}
-                                </label>
-                            </div>
-                        </>
-                    )}
-                </div>
-                <div className={kcClsx("kcFormGroupClass")}>
-                    <div id="kc-form-options" className={kcClsx("kcFormOptionsClass")}>
-                        <div className={kcClsx("kcFormOptionsWrapperClass")} />
-                    </div>
-                    <div id="kc-form-buttons" className={kcClsx("kcFormButtonsClass")}>
-                        <div className={kcClsx("kcFormButtonsWrapperClass")}>
-                            <input
-                                className={kcClsx("kcButtonClass", "kcButtonPrimaryClass", "kcButtonLargeClass")}
-                                name="login"
-                                id="kc-login"
-                                type="submit"
-                                value={msgStr("doContinue")}
-                            />
-                            {x509.formData.isUserEnabled && (
-                                <input
-                                    className={kcClsx("kcButtonClass", "kcButtonDefaultClass", "kcButtonLargeClass")}
-                                    name="cancel"
-                                    id="kc-cancel"
-                                    type="submit"
-                                    value={msgStr("doIgnore")}
-                                />
-                            )}
-                        </div>
-                    </div>
-                </div>
+                    </>
+                )}
+            </div>
+            <form id="kc-x509-login-info" className={clsx(kcClsx("kcFormClass"), "formSpaceClass")} action={url.loginAction} method="post">
+                <ButtonGroup>
+                    <>
+                        <SubmitButton doUseDefaultCss={doUseDefaultCss} classes={classes} name="login" id="kc-login">
+                            {msg("doContinue")}
+                        </SubmitButton>
+                        {x509.formData.isUserEnabled && (
+                            <SubmitButton
+                                doUseDefaultCss={doUseDefaultCss}
+                                classes={classes}
+                                id="kc-cancel"
+                                name="cancel"
+                                colorClass="buttonSecondaryClass"
+                            >
+                                {msg("doIgnore")}
+                            </SubmitButton>
+                        )}
+                    </>
+                </ButtonGroup>
             </form>
         </Template>
     );
