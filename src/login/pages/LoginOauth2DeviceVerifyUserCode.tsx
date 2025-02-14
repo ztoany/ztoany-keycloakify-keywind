6240b25ec44e498ba1646c9d1b6c5cb2b46465ac
ztoany <marvin.m.huang@outlook.com>
2025-02-14 13:20:45 +0800

    apply keywind to LoginOauth2DeviceVerifyUserCode

--- src/login/pages/LoginOauth2DeviceVerifyUserCode.tsx
+++ src/login/pages/LoginOauth2DeviceVerifyUserCode.tsx
@@ -1,6 +1,10 @@
 import { getKcClsx } from "keycloakify/login/lib/kcClsx";
 import { PageProps } from "keycloakify/login/pages/PageProps";
+import { clsx } from "keycloakify/tools/clsx";
 import { KcContext } from "../KcContext";
+import ButtonGroup from "../components/ButtonGroup";
+import FormInput from "../components/FormInput";
+import SubmitButton from "../components/SubmitButton";
 import type { I18n } from "../i18n";
 
 export default function LoginOauth2DeviceVerifyUserCode(
@@ -26,44 +30,29 @@ export default function LoginOauth2DeviceVerifyUserCode(
         >
             <form
                 id="kc-user-verify-device-user-code-form"
-                className={kcClsx("kcFormClass")}
+                className={clsx(kcClsx("kcFormClass"), "formSpaceClass")}
                 action={url.oauth2DeviceVerificationAction}
                 method="post"
             >
-                <div className={kcClsx("kcFormGroupClass")}>
-                    <div className={kcClsx("kcLabelWrapperClass")}>
-                        <label htmlFor="device-user-code" className={kcClsx("kcLabelClass")}>
-                            {msg("verifyOAuth2DeviceUserCode")}
-                        </label>
-                    </div>
-
-                    <div className={kcClsx("kcInputWrapperClass")}>
-                        <input
-                            id="device-user-code"
-                            name="device_user_code"
-                            autoComplete="off"
-                            type="text"
-                            className={kcClsx("kcInputClass")}
-                            autoFocus
-                        />
-                    </div>
-                </div>
-
-                <div className={kcClsx("kcFormGroupClass")}>
-                    <div id="kc-form-options" className={kcClsx("kcFormOptionsClass")}>
-                        <div className={kcClsx("kcFormOptionsWrapperClass")}></div>
-                    </div>
-
-                    <div id="kc-form-buttons" className={kcClsx("kcFormButtonsClass")}>
-                        <div className={kcClsx("kcFormButtonsWrapperClass")}>
-                            <input
-                                className={kcClsx("kcButtonClass", "kcButtonPrimaryClass", "kcButtonLargeClass")}
-                                type="submit"
-                                value={msgStr("doSubmit")}
-                            />
-                        </div>
-                    </div>
-                </div>
+                <FormInput
+                    doUseDefaultCss={doUseDefaultCss}
+                    classes={classes}
+                    id="device-user-code"
+                    name="device_user_code"
+                    autoComplete="off"
+                    type="text"
+                    invalid={false}
+                    autoFocus={true}
+                    errorMsg=""
+                    errorId="input-error"
+                    placeholder={msgStr("verifyOAuth2DeviceUserCode")}
+                    labelContent={msgStr("verifyOAuth2DeviceUserCode")}
+                ></FormInput>
+                <ButtonGroup>
+                    <SubmitButton doUseDefaultCss={doUseDefaultCss} classes={classes}>
+                        {msg("doSubmit")}
+                    </SubmitButton>
+                </ButtonGroup>
             </form>
         </Template>
     );
